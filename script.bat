a0666597689fe9db441307445647401a23701f2c
restorimo <restorimo@protonmail.com>
2023-06-19 17:11:24 +0200

    output numlist

--- script.bat
+++ script.bat
@@ -54,43 +54,54 @@ powershell -Command "$content = Get-Content -Encoding UTF8 list.txt; Set-Content
 REM Step 11: Count the lines in list.txt
 for /f %%a in ('type list.txt ^| find /c /v ""') do set "lineCount=%%a"
 
-REM Step 12: Get output format
-set "outputFormat=gif"
-set /p "outputFormat=Enter the output format (gif, h264, h265, h264nvidia, h265nvidia, vp8, vp9, av1, default is gif): "
-if "%outputFormat%"=="h264" (
+REM Step 12: Display selectable numeric list for output format
+echo Available Output Formats:
+echo 1 - GIF
+echo 2 - H264
+echo 3 - H265
+echo 4 - H264NVIDIA
+echo 5 - H265NVIDIA
+echo 6 - VP8
+echo 7 - VP9
+echo 8 - AV1
+set "outputFormat=1"
+set /p "outputFormat=Enter the output format number (default is 1): "
+
+REM Step 13: Map output format number to corresponding codec and output extension
+if "%outputFormat%"=="2" (
     set "codec=libx264"
     set "outputExtension=mp4"
-) else if "%outputFormat%"=="h265" (
+) else if "%outputFormat%"=="3" (
     set "codec=libx265"
     set "outputExtension=mp4"
-) else if "%outputFormat%"=="h264nvidia" (
+) else if "%outputFormat%"=="4" (
     set "codec=h264_nvenc"
     set "outputExtension=mp4"
-) else if "%outputFormat%"=="h265nvidia" (
+) else if "%outputFormat%"=="5" (
     set "codec=hevc_nvenc"
     set "outputExtension=mp4"
-) else if "%outputFormat%"=="vp8" (
+) else if "%outputFormat%"=="6" (
     set "codec=libvpx"
     set "outputExtension=webm"
-) else if "%outputFormat%"=="vp9" (
+) else if "%outputFormat%"=="7" (
     set "codec=libvpx-vp9"
     set "outputExtension=webm"
-) else if "%outputFormat%"=="av1" (
+) else if "%outputFormat%"=="8" (
     set "codec=libaom-av1"
     set "outputExtension=webm"
 ) else (
-    echo Invalid output format selected. Defaulting to GIF.
+    echo Invalid output format number selected. Defaulting to GIF.
     set "codec=gif"
     set "outputExtension=gif"
 )
 
-REM Step 12: Calculate the duration based on line count and frame rate
+REM Step 14: Calculate the duration based on line count and frame rate
 set /a "duration=lineCount / frameRate"
 
-REM Step 13: Generate output file name
+REM Step 15: Generate output file name
 set "outputName=%outputName%.%outputExtension%"
 
-REM Step 14: Generate output using ffmpeg with variables
+REM Step 16: Generate output using ffmpeg with variables
 ffmpeg -stream_loop -1 -f concat -safe 0 -i list.txt -loop 1 -i overlay.png -filter_complex "[0:v]scale=-1:%scaleInput%:force_original_aspect_ratio=decrease,pixelize=%pixelizeInput%:%pixelizeInput%,noise=alls=%noiseInput%:allf=t+u[bg];[bg][1:v]overlay=(W-w)/2:(H-h)/2:enable='between(t,0,%duration%)':shortest=1" -c:v %codec% -r %frameRate% -t %duration% %outputName%
 
 REM Cleanup: Remove list.txt file
